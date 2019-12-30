pevik/libtirpc

commit bf8f0b82df20eb70cca557b4c8bc42b00b0ab73f

Add back the authdes interfaces
Instead of compiling out the authdes interfaces
as commit d918e41d8 did, add them back but
they will fail if called.

Signed-off-by: Steve Dickson <steved@redhat.com>

src/rpc_soc.c
@@ -595,6 +595,24 @@ authdes_pk_create(servername, pkey, window, syncaddr, ckey)
 fallback:
 	return authdes_pk_seccreate(servername, pkey, window, NULL, ckey, NULL);
 }
+#else
+AUTH *
+authdes_create(servername, window, syncaddr, ckey)
+	char *servername;		/* network name of server */
+	u_int window;			/* time to live */
+	struct sockaddr *syncaddr;	/* optional hostaddr to sync with */
+	des_block *ckey;		/* optional conversation key to use */
+{ return (NULL); }
+
+AUTH *
+authdes_pk_create(servername, pkey, window, syncaddr, ckey)
+	char *servername;		/* network name of server */
+	netobj *pkey;			/* public key */
+	u_int window;			/* time to live */
+	struct sockaddr *syncaddr;	/* optional hostaddr to sync with */
+	des_block *ckey;		/* optional conversation key to use */
+{ return (NULL); }
+
 #endif
 
 

src/svc_auth.c
@@ -114,10 +114,12 @@ _gss_authenticate(rqst, msg, no_dispatch)
 	case AUTH_SHORT:
 		dummy = _svcauth_short(rqst, msg);
 		return (dummy);
-#ifdef AUTHDES_SUPPORT
 	case AUTH_DES:
+#ifdef AUTHDES_SUPPORT
 		dummy = _svcauth_des(rqst, msg);
 		return (dummy);
+#else
+		return (AUTH_FAILED);
 #endif
 #ifdef HAVE_RPCSEC_GSS
 	case RPCSEC_GSS:
@@ -178,6 +180,9 @@ svc_auth_reg(cred_flavor, handler)
 	    case AUTH_SYS:
 	    case AUTH_SHORT:
 	    case AUTH_DES:
+#ifndef AUTHDES_SUPPORT
+		return(-1);
+#endif
 #ifdef HAVE_RPCSEC_GSS
 	    case RPCSEC_GSS:
 #endif

commit 1bbcd445f81e9826468248d8ccbbddbe8f6c0440

Add authdes_seccreate() stub
bf8f0b82d added back authdes_create() and authdes_pk_create() interfaces
also when authdes compiled out. Add also authdes_seccreate().

Found by LTP rpc-tirpc test, which fails to link:

/usr/lib64/gcc/x86_64-suse-linux/9/../../../../x86_64-suse-linux/bin/ld: /tmp/ccFanCMm.o: in function `main':
    testcases/network/rpc/rpc-tirpc/tests_pack/rpc_suite/tirpc/tirpc_auth_authdes_seccreate/tirpc_authdes_seccreate.c:55: undefined reference to `authdes_seccreate'

Fixes: bf8f0b82d ("Add back the authdes interfaces")

Signed-off-by: Petr Vorel <pvorel@suse.cz>

src/rpc_soc.c
@@ -613,6 +613,13 @@ authdes_pk_create(servername, pkey, window, syncaddr, ckey)
 	des_block *ckey;		/* optional conversation key to use */
 { return (NULL); }
 
+AUTH *
+authdes_seccreate(const char *servername, const u_int win,
+	const char *timehost, const des_block *ckey)
+{
+	return (NULL);
+}
+
 #endif
 
 